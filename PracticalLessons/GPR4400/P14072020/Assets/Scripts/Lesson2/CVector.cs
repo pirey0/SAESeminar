aede7e2c31b4dfb43d061506ce1badff47d8e3a7
Luca Martinelli <lucxmangajet@gmail.com>
2020-09-21 17:57:35 +0200

    lesson 2 gpr4400

---
+++ PracticalLessons/GPR4400/P14072020/Assets/Scripts/Lesson2/CVector.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct CVector
+{
+
+    //magnitude
+    //normalized
+    //addition
+    //multipication by scalar
+    //dot product
+    //cross product
+
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+
+    public float Magnitdue
+    {
+        get => (float)Math.Sqrt(X * X + Y * Y + Z * Z);
+    }
+
+    public CVector Normalized
+    {
+        get => Normalize(this);
+    }
+
+    public CVector(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static CVector operator +(CVector v1, CVector v2)
+    {
+        return new CVector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+    }
+
+    public static CVector operator *(CVector v, float s)
+    {
+        return new CVector(v.X * s, v.Y * s, v.Z * s);
+    }
+
+    public static CVector operator *(float s, CVector v)
+    {
+        return v * s;
+    }
+
+    public static CVector Normalize(CVector v)
+    {
+        var m = 1 / v.Magnitdue;
+        return new CVector(v.X * m, v.Y * m, v.Z * m);
+    }
+
+    public static float Dot(CVector v1, CVector v2)
+    {
+        return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+    }
+
+    public static CVector Cross(CVector v1, CVector v2)
+    {
+        float x = v1.Y * v2.Z - v2.Y * v1.Z;
+        float y = v1.Z * v2.X - v2.Z * v1.X;
+        float z = v1.X * v2.Y - v2.X * v1.Y;
+
+        return new CVector(x, y, z);
+    }
+
+}
---
+++ PracticalLessons/GPR4400/P14072020/Assets/Scripts/Lesson2/VectorVisualizer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class VectorVisualizer : MonoBehaviour
+{
+    [Range(0, 1)]
+    [SerializeField] float headLength;
+
+    [Range(1, 90)]
+    [SerializeField] float headAngle;
+
+    [SerializeField] Arrow[] arrows;
+
+
+    private void OnDrawGizmos()
+    {
+        
+        if (arrows == null)
+            return;
+
+        foreach (var arrow in arrows)
+        {
+            if (arrow.Direction.magnitude > 0)
+                DrawArrow(arrow, headLength, headAngle);
+        }
+    }
+
+    public static void DrawArrow(Arrow arrow, float headLength, float headAngle)
+    {
+        DrawArrow(arrow.Position, arrow.Direction, arrow.Color, headLength, headAngle);
+    }
+
+    public static void DrawArrow(Vector3 pos, Vector3 direction, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawRay(pos, direction);
+
+        Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Vector3 up = Quaternion.LookRotation(direction) * Quaternion.Euler(180 + arrowHeadAngle, 0, 0) * new Vector3(0, 0, 1);
+        Vector3 down = Quaternion.LookRotation(direction) * Quaternion.Euler(180 - arrowHeadAngle, 0, 0) * new Vector3(0, 0, 1);
+        Gizmos.DrawRay(pos + direction, right * arrowHeadLength);
+        Gizmos.DrawRay(pos + direction, left * arrowHeadLength);
+        Gizmos.DrawRay(pos + direction, up * arrowHeadLength);
+        Gizmos.DrawRay(pos + direction, down * arrowHeadLength);
+
+    }
+
+}
+
+[System.Serializable]
+public class Arrow
+{
+    public Vector3 Position;
+    public Vector3 Direction = Vector3.right;
+    public Color Color = Color.white;
+}
