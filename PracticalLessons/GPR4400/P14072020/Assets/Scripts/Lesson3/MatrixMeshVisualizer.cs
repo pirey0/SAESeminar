93373e0c0c223f133c0e5724816b28223e574d95
Luca Martinelli <lucxmangajet@gmail.com>
2020-09-24 15:29:42 +0200

    added rendering representation

---
+++ PracticalLessons/GPR4400/P14072020/Assets/Scripts/Lesson3/MatrixMeshVisualizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixMeshVisualizer : MonoBehaviour
+{
+    [SerializeField] Mesh mesh;
+
+    [SerializeField] Matrix4x4 matrixGreen = Matrix4x4.identity, matrixRed = Matrix4x4.identity;
+
+
+    private void OnDrawGizmos()
+    {
+        var gizmosRenderingMatrix = Gizmos.matrix;
+
+        Gizmos.matrix = matrixGreen;
+        Gizmos.color = Color.green;
+        Gizmos.DrawMesh(mesh, transform.position);
+
+        Gizmos.matrix = matrixRed;
+        Gizmos.color = Color.red;
+        Gizmos.DrawMesh(mesh, transform.position);
+
+        Gizmos.matrix = gizmosRenderingMatrix;
+    }
+}
