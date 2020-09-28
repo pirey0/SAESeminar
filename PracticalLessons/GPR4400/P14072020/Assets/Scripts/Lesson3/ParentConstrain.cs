f21c743206fce5ca45da875c6f33f638c43a0466
Luca Martinelli <lucxmangajet@gmail.com>
2020-09-28 11:17:37 +0200

    added parent constrain visualization

---
+++ PracticalLessons/GPR4400/P14072020/Assets/Scripts/Lesson3/ParentConstrain.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParentConstrain : MonoBehaviour
+{
+
+    [SerializeField] Transform parent, child;
+
+    [SerializeField] Matrix4x4 parentMatrix;
+    [SerializeField] Matrix4x4 childMatrix;
+
+    [SerializeField] bool applyParent;
+
+    private void Update()
+    {
+        if (parent == null || child == null)
+            return;
+
+        parent.localRotation = MatrixHelper.GetRotation(parentMatrix);
+        parent.localScale = MatrixHelper.GetScale(parentMatrix);
+        parent.localPosition = MatrixHelper.GetPosition(parentMatrix);
+
+
+        Matrix4x4 childMatrixTransformed = childMatrix;
+
+        if (applyParent)
+            childMatrixTransformed = childMatrix * parentMatrix;
+
+
+        child.localRotation = MatrixHelper.GetRotation(childMatrixTransformed);
+        child.localScale = MatrixHelper.GetScale(childMatrixTransformed);
+        child.localPosition = MatrixHelper.GetPosition(childMatrixTransformed);
+    }
+
+}
