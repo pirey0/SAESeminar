b1059d57af3eedda43e684942366761ca35be315
Luca Martinelli <lucxmangajet@gmail.com>
2020-09-28 15:11:31 +0200

    added projection visualizer

---
+++ PracticalLessons/GPR4400/P14072020/Assets/Scripts/Lesson3/ProjectionVisualizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectionVisualizer : MonoBehaviour
+{
+    [SerializeField] Camera camera;
+    [SerializeField] Mesh toRender;
+    [SerializeField] Matrix4x4 matrix;
+
+    private void OnDrawGizmos()
+    {
+        if (camera == null)
+            return;
+
+        matrix = camera.previousViewProjectionMatrix;
+        Gizmos.matrix = matrix;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawMesh(toRender, new Vector3(0,0,5));
+        Gizmos.matrix = Matrix4x4.identity;
+    }
+}
