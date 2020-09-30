f72a9496df3a220b7f5601aaccdd5d32366b20ca
Luca Martinelli <lucxmangajet@gmail.com>
2020-09-30 13:17:30 +0200

    added monster battle simulator code

---
+++ PracticalLessons/GPR4100/MonsterBattleSimulator/MonsterBattleSimulator/Monster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MonsterBattleSimulator
+{
+    class Monster
+    {
+        public float Health;
+        public float AttackPower;
+        public float DefensePower;
+        public float Speed;
+
+        public Race Race;
+
+        public void Attack(Monster target)
+        {
+            float damage = this.AttackPower - target.DefensePower;
+
+            if (damage < 0)
+                damage = 0;
+
+            target.Health -= damage;
+        }
+
+
+
+    }
+
+    //integer with a name
+    public enum Race
+    {
+        Orc = 1,
+        Troll,
+        Goblin 
+    }
+}
---
+++ PracticalLessons/GPR4100/MonsterBattleSimulator/MonsterBattleSimulator/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterBattleSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monster m1 =  GetMonsterFromUser();
+
+            Console.WriteLine("Monster 1 done! Now defining monster 2..");
+            Monster m2 = GetMonsterFromUser(); 
+            Console.ReadLine();
+
+        }
+
+        private static Monster GetMonsterFromUser()
+        {
+            Console.WriteLine("Creating Monster:");
+            Race race1 = GetRaceFromUser();
+
+            Console.WriteLine("HP:");
+            float hp = GetNumberFromUser();
+
+            Console.WriteLine("Attack Power:");
+            float ap = GetNumberFromUser();
+
+            Console.WriteLine("Defensive Power:");
+            float dp = GetNumberFromUser();
+
+            Console.WriteLine("Speed:");
+            float sp = GetNumberFromUser();
+
+
+            Monster monster1 = new Monster();
+            monster1.Race = race1;
+            monster1.Health = hp;
+            monster1.AttackPower = ap;
+            monster1.DefensePower = dp;
+            monster1.Speed = sp;
+
+            return monster1;
+        }
+
+        private static float GetNumberFromUser()
+        {
+            int result;
+            do
+            {
+                Console.WriteLine("Please pick a Number between 0 and 9");
+                ConsoleKey key = Console.ReadKey().Key;
+                result = (int)key - (int)ConsoleKey.D0;
+
+            }
+            while (result < 0 || result > 9);
+
+            Console.WriteLine();
+            Console.WriteLine("Selected: " + result);
+            return result;
+        }
+
+        private static Race GetRaceFromUser()
+        {
+            int keyRace;
+            do
+            {
+                Console.WriteLine("Please pick a Race: (1=Orc, 2=Troll, 3=Goblin)");
+                ConsoleKey key = Console.ReadKey().Key;
+                keyRace = (int)key - (int)ConsoleKey.D0;
+
+            }
+            while (keyRace < 0 || keyRace > 3);
+
+            Race raceInput;
+            raceInput = (Race)keyRace;
+            
+            Console.WriteLine();
+            Console.WriteLine("You selected a " + raceInput);
+            return raceInput;
+        }
+
+        private static void TestAttack()
+        {
+            Monster monster1 = new Monster();
+            Monster monster2 = new Monster();
+
+            monster1.AttackPower = 5;
+            monster2.DefensePower = 2;
+            monster2.Health = 10;
+
+            monster1.Attack(monster2);
+
+            Console.WriteLine("TestAttack Result: " + (monster2.Health == 7));
+        }
+    }
+}
