3a2dba40fb0b56e6f24ae48a3aaa038a499ba473
Luca Martinelli <lucxmangajet@gmail.com>
2020-09-30 18:33:54 +0200

    added asteroid base for 4100

---
+++ PracticalLessons/GPR4100/Asteroids/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed;
+    [SerializeField] Vector3 movementVector;
+
+    public float boundsRange;
+
+    private void Update()
+    {
+        if (IsOutsideBounds())
+        {
+            transform.position = -transform.position;
+        }
+
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        transform.position += movementVector * Time.deltaTime;
+
+       
+    }
+
+    public bool IsOutsideBounds()
+    {
+        return transform.position.magnitude > boundsRange;
+    }
+
+}
