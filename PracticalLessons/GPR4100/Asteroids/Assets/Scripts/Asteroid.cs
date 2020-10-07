780eaa4cb29abb04d8681159081057f22b9369d2
Luca Martinelli <lucxmangajet@gmail.com>
2020-10-07 11:52:18 +0200

    added basic asteroids

--- PracticalLessons/GPR4100/Asteroids/Assets/Scripts/Asteroid.cs
+++ PracticalLessons/GPR4100/Asteroids/Assets/Scripts/Asteroid.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    [SerializeField] float rotationSpeed;
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] private float _rotationMaxSpeed = 90;
+    [SerializeField] private float _maximumMovmentSpeed = 1;
+    [SerializeField] private Sprite[] _asteroidSprites;
 
-    public float boundsRange;
+
+    private float _randomRotationSpeed;
+    private Vector3 _randomMovementDirection;
+
+    private void Start()
+    {
+        _randomRotationSpeed = Random.Range(-1f, 1f);
+        _randomMovementDirection = Random.insideUnitCircle;
+
+        if (_asteroidSprites != null && _asteroidSprites.Length > 0)
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = _asteroidSprites[Random.Range(0, _asteroidSprites.Length)];
+        }
+    }
 
     private void Update()
     {
-        if (IsOutsideBounds())
-        {
-            transform.position = -transform.position;
-        }
-
-        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
-        transform.position += movementVector * Time.deltaTime;
-
-       
-    }
-
-    public bool IsOutsideBounds()
-    {
-        return transform.position.magnitude > boundsRange;
+        transform.eulerAngles += new Vector3(0, 0, Time.deltaTime * _randomRotationSpeed * _rotationMaxSpeed);
+        transform.position += _randomMovementDirection * _maximumMovmentSpeed * Time.deltaTime;
     }
 
 }
---
+++ PracticalLessons/GPR4100/Asteroids/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _asteroidPrefab;
+
+    [SerializeField] private float _delayBetweenSpawn;
+
+    [SerializeField] private float _spawnRange = 10;
+
+    private float _timer = 1;
+
+    private void Update()
+    {
+        if(_timer <= 0)
+        {
+            //spawn asteroid
+            GameObject spawnedObject = Instantiate(_asteroidPrefab);
+            spawnedObject.transform.position = Random.insideUnitCircle * _spawnRange;
+
+            _timer = _delayBetweenSpawn;
+        }
+        else
+        {
+            _timer -= Time.deltaTime;
+        }
+    }
+}
