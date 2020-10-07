c0c3071da29d2dbe434f2e0c7d3dc85a4c83ea4c
Luca Martinelli <lucxmangajet@gmail.com>
2020-10-07 13:29:56 +0200

    added german asteroid

---
+++ PracticalLessons/GPR4100/AsteroidsDE/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float MaxRotationSpeed;
+
+    [SerializeField] private Vector3 velocity;
+
+    private float randomRotationSpeed;
+
+    private void Start()
+    {
+        randomRotationSpeed = Random.Range(-1f, 1);
+        velocity = Random.insideUnitCircle;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, randomRotationSpeed * MaxRotationSpeed * Time.deltaTime);
+        transform.position += velocity * Time.deltaTime;
+
+    }
+}
