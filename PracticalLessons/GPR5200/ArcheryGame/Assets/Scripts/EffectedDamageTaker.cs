a20b2e2023c238056ac8485b81538554dc0bfbbc
Luca Martinelli <lucxmangajet@gmail.com>
2020-10-15 13:06:14 +0200

    added fire effect, fixed error

---
+++ PracticalLessons/GPR5200/ArcheryGame/Assets/Scripts/EffectedDamageTaker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectedDamageTaker : MonoBehaviour, IEffectedDamagable
+{
+    public bool IgnoresEffect(string effectName)
+    {
+        return false;
+    }
+
+    public TakeDamageResult TakeDamage(float damageAmount)
+    {
+        Debug.Log(name + " took " + damageAmount + " damage");
+        return TakeDamageResult.Destroy;
+    }
+}
---
+++ PracticalLessons/GPR5200/ArcheryGame/Assets/Scripts/Effects/SpawnObjectEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectEffect : EffectDefinitionComponet
+{
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] float lifeTime;
+    [SerializeField] bool oriented;
+
+    protected override void ApplyEffect(EffectParams parameters)
+    {
+        var go = Instantiate(objectToSpawn, parameters.Target.transform.position, oriented? parameters.Target.transform.rotation : Quaternion.identity);
+        if(lifeTime > 0)
+        {
+            Destroy(go, lifeTime);
+        }
+    }
+}
