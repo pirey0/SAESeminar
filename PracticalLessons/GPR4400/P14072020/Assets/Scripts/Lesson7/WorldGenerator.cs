762eb7ba913c94afc0513cad03c1d70ec1e3f52c
Luca Martinelli <lucxmangajet@gmail.com>
2020-11-02 17:40:50 +0100

    added AStar

--- PracticalLessons/GPR4400/P14072020/Assets/Scripts/Lesson7/WorldGenerator.cs
+++ PracticalLessons/GPR4400/P14072020/Assets/Scripts/Lesson7/WorldGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
@@ -18,7 +19,7 @@ public class WorldGenerator : MonoBehaviour
         map = new TileType[size, size];
 
 
-        for (int i = 0; i < size*Mathf.Log(size); i++)
+        for (int i = 0; i < size * Mathf.Log(size); i++)
         {
             int x = UnityEngine.Random.Range(0, size);
             int y = UnityEngine.Random.Range(0, size);
@@ -34,9 +35,20 @@ public class WorldGenerator : MonoBehaviour
             map[i, size - 1] = TileType.Wall;
         }
 
-
         SpawnVisuals();
 
+        var path = AStar(startLocation, endLocation, AirDistance);
+        
+        if(path != null)
+        {
+            foreach (var p in path)
+            {
+                var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.transform.localScale = Vector3.one * 0.5f;
+                go.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                go.transform.position = new Vector3(p.x, 0, p.y);
+            }
+        }
     }
 
     private void SpawnVisuals()
@@ -63,6 +75,104 @@ public class WorldGenerator : MonoBehaviour
         goal.position = new Vector3(endLocation.x, 0, endLocation.y);
     }
 
+    private Vector2Int[] ReconstructPath(Dictionary<Vector2Int, Vector2Int> cameFrom, Vector2Int current)
+    {
+        Queue<Vector2Int> totalPath = new Queue<Vector2Int>();
+        totalPath.Enqueue(current);
+
+        while (cameFrom.ContainsKey(current))
+        {
+            current = cameFrom[current];
+            totalPath.Enqueue(current);
+        }
+
+        return totalPath.ToArray();
+    }
+
+
+    private float AirDistance(Vector2Int from, Vector2Int to)
+    {
+        return Vector2Int.Distance(from, to);
+    }
+
+    delegate float HeuristicDelegate(Vector2Int v1, Vector2Int v2);
+
+    private Vector2Int[] AStar(Vector2Int start, Vector2Int goal, HeuristicDelegate heuristic)
+    {
+        List<Vector2Int> openSet = new List<Vector2Int>();
+        openSet.Add(start);
+
+        Dictionary<Vector2Int, Vector2Int> cameFrom = new Dictionary<Vector2Int, Vector2Int>();
+
+        Dictionary<Vector2Int, float> gScore = new Dictionary<Vector2Int, float>();
+        InitiateToInfinity(ref gScore);
+        gScore[start] = 0;
+
+        Dictionary<Vector2Int, float> fScore = new Dictionary<Vector2Int, float>();
+        InitiateToInfinity(ref fScore);
+        fScore[start] = heuristic(start, goal);
+
+        while (openSet.Count > 0)
+        {
+            openSet.Sort((a, b) => (int)fScore[a] - (int)fScore[b]);
+            Vector2Int current = openSet[0];
+
+            if (current == goal)
+                return ReconstructPath(cameFrom, current);
+
+            openSet.Remove(current);
+
+            foreach (var neighbour in GetNeighboursOf(current))
+            {
+                float tentativeGScore = gScore[current] + GetEdgeWeightBetween(current, neighbour);
+
+                if (tentativeGScore < gScore[neighbour])
+                {
+                    if (cameFrom.ContainsKey(neighbour))
+                        cameFrom[neighbour] = current;
+                    else
+                        cameFrom.Add(neighbour, current);
+
+                    gScore[neighbour] = tentativeGScore;
+                    fScore[neighbour] = tentativeGScore + heuristic(neighbour, goal);
+                    if (!openSet.Contains(neighbour))
+                    {
+                        openSet.Add(neighbour);
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    float GetEdgeWeightBetween(Vector2Int current, Vector2Int neighbour)
+    {
+        if (map[current.x, current.y] == TileType.Wall)
+            return float.MaxValue;
+        else if (map[neighbour.x, neighbour.y] == TileType.Wall)
+            return float.MaxValue;
+
+        return 1;
+    }
+
+    Vector2Int[] GetNeighboursOf(Vector2Int node)
+    {
+        return new Vector2Int[] { new Vector2Int(node.x +1, node.y), new Vector2Int(node.x -1 , node.y),
+            new Vector2Int(node.x, node.y+1), new Vector2Int(node.x, node.y-1) };
+    }
+
+    void InitiateToInfinity(ref Dictionary<Vector2Int, float> dict)
+    {
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                dict.Add(new Vector2Int(x, y), float.MaxValue);
+            }
+        }
+    }
+
 }
 
 public enum TileType
@@ -70,3 +180,4 @@ public enum TileType
     Floor,
     Wall
 }
+
