fac09ebdf70c23e98aa6820506d88d8d1594a87d
Luca Martinelli <lucxmangajet@gmail.com>
2020-12-10 10:41:46 +0100

    added CS version of C# to Cpp practice

---
+++ PracticalLessons0912/GPR5300/CsToCppExample/CsOriginal/CsOriginal/Engine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsCalc
+{
+    class Engine
+    {
+        int size;
+        Vector2Int playerPosition;
+
+        public Engine(int size)
+        {
+            this.size = size;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            playerPosition.x = (playerPosition.x + dx + size) % size;
+            playerPosition.y = (playerPosition.y + dy + size) % size;
+        }
+
+        public string Draw()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    sb.Append((x == playerPosition.x && y == playerPosition.y) ? "O" : "_");
+                }
+                sb.Append(Environment.NewLine);
+            }
+
+            return sb.ToString();
+        }
+
+    }
+
+    public struct Vector2Int
+    {
+        public int x;
+        public int y;
+
+        public Vector2Int(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector2Int operator + (Vector2Int lhs, Vector2Int rhs)
+        {
+            return new Vector2Int(lhs.x + rhs.x, lhs.y + rhs.y);
+        }
+    }
+}
---
+++ PracticalLessons0912/GPR5300/CsToCppExample/CsOriginal/CsOriginal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CsCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine(10);
+
+            while (true)
+            {
+                Console.Clear();
+                string draw = engine.Draw();
+                Console.WriteLine(draw);
+
+
+                var key = Console.ReadKey().Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.A:
+                        engine.Move(-1, 0);
+                        break;
+
+                    case ConsoleKey.D:
+                        engine.Move(1, 0);
+                        break;
+
+                    case ConsoleKey.W:
+                        engine.Move(0, -1);
+                        break;
+
+                    case ConsoleKey.S:
+                        engine.Move(0, 1);
+                        break;
+                }
+            }
+        }
+    }
+}
