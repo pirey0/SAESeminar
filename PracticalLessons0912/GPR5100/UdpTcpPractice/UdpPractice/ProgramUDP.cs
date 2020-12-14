4a3ca4a5dee8bf4d1fc0cbfc6e01353a3c7dc34d
Luca Martinelli <lucxmangajet@gmail.com>
2020-12-14 16:36:36 +0100

    changed for online UDP

--- PracticalLessons0912/GPR5100/UdpTcpPractice/UdpPractice/ProgramUDP.cs
+++ PracticalLessons0912/GPR5100/UdpTcpPractice/UdpPractice/ProgramUDP.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -18,42 +19,52 @@ namespace UdpPractice
             udpClient = new UdpClient(new IPEndPoint(IPAddress.Loopback, 0)); // 127.0.0.1
             Console.WriteLine("Socket bound to: " + udpClient.Client.LocalEndPoint.ToString());
 
-            Console.WriteLine("Write target local port: ");
-            string targetPort = Console.ReadLine();
-            
-            if(int.TryParse(targetPort, out int result))
+            Console.WriteLine("Write target address: ");
+            string targetAddress = Console.ReadLine();
+
+            CreateIPEndPoint(targetAddress);
+            targetEndPoint = CreateIPEndPoint(targetAddress);
+
+            Console.WriteLine("Endpoint set to: " + targetEndPoint.ToString());
+
+            //Start asyncronous thread to receive data
+            Task.Run(AsyncReceiveData);
+
+            while (true)
             {
-                targetEndPoint = new IPEndPoint(IPAddress.Loopback, result);
-                Console.WriteLine("Endpoint set to: " + targetEndPoint.ToString());
-
-                //Start asyncronous thread to receive data
-                Task.Run(AsyncReceiveData);
-
-                while (true)
-                {
-                    string message = Console.ReadLine();
-                    var messageBinary = Encoding.ASCII.GetBytes(message);
-                    udpClient.Send(messageBinary, messageBinary.Length, targetEndPoint);
-                }
-
+                string message = Console.ReadLine();
+                var messageBinary = Encoding.ASCII.GetBytes(message);
+                udpClient.Send(messageBinary, messageBinary.Length, targetEndPoint);
             }
-            else
+        }
+
+        public static IPEndPoint CreateIPEndPoint(string endPoint)
+        {
+            string[] ep = endPoint.Split(':');
+            if (ep.Length != 2) throw new FormatException("Invalid endpoint format");
+            IPAddress ip;
+            if (!IPAddress.TryParse(ep[0], out ip))
             {
-                Console.WriteLine("Failed parsing port.");
-                Console.ReadLine();
+                throw new FormatException("Invalid ip-adress");
             }
+            int port;
+            if (!int.TryParse(ep[1], NumberStyles.None, NumberFormatInfo.CurrentInfo, out port))
+            {
+                throw new FormatException("Invalid port");
+            }
+            return new IPEndPoint(ip, port);
         }
 
         private static async void AsyncReceiveData()
         {
-            while(true)
+            while (true)
             {
                 UdpReceiveResult result = await udpClient.ReceiveAsync();
-                Console.WriteLine(result.RemoteEndPoint.ToString() +": " + Encoding.ASCII.GetString(result.Buffer));
+                Console.WriteLine(result.RemoteEndPoint.ToString() + ": " + Encoding.ASCII.GetString(result.Buffer));
             }
         }
 
 
-        
+
     }
 }
