2f11df121c8bbd08fe880764aed2687cf8f5ba62
Luca Martinelli <lucxmangajet@gmail.com>
2021-02-03 00:33:20 +0100

    added drag and drop behaviour for practice

---
+++ PracticalLessons0912/GPR4300/4300Unity/Assets/Scripts/MovableObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        if (rigidbody == null)
+            rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public virtual void Pickup()
+    {
+        if (rigidbody != null)
+            rigidbody.isKinematic = true;
+    }
+
+    public virtual void MoveTo(Vector3 newPos)
+    {
+        transform.position = newPos;
+    }
+    public virtual void Release()
+    {
+        if (rigidbody != null)
+            rigidbody.isKinematic = false;
+    }
+}
---
+++ PracticalLessons0912/GPR4300/4300Unity/Assets/Scripts/PlayerMoveHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerMoveHandler : MonoBehaviour
+{
+    MovableObject currentObject;
+    Camera camera;
+
+    Plane pickupPlane;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TryPickup();
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            UpdatePickup();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            TryRelease();
+        }
+    }
+
+    private void UpdatePickup()
+    {
+        if (currentObject != null)
+        {
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (pickupPlane.Raycast(ray, out float dist))
+            {
+                Vector3 newPos = ray.GetPoint(dist);
+                currentObject.MoveTo(newPos);
+            }
+        }
+    }
+
+    private void TryRelease()
+    {
+        if (currentObject != null)
+        {
+            currentObject.Release();
+            currentObject = null;
+        }
+    }
+
+    private void TryPickup()
+    {
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        var hits = Physics.RaycastAll(ray, 100);
+
+        foreach (var h in hits)
+        {
+            if (h.transform.TryGetComponent(out MovableObject movableObject))
+            {
+                currentObject = movableObject;
+                currentObject.Pickup();
+
+                pickupPlane = new Plane(Vector3.forward, h.point);
+                break;
+            }
+        }
+    }
+}
