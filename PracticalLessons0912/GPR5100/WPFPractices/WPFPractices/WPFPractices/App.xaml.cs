0b32ee93514776bd4a50938022f58a9fee630ae3
Luca Martinelli <lucxmangajet@gmail.com>
2021-02-03 21:14:36 +0100

    added 5100WPF practice

---
+++ PracticalLessons0912/GPR5100/WPFPractices/WPFPractices/WPFPractices/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFPractices
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+    }
+}
