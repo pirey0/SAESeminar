1be6e995165cb65936eeff3709ccb1ed50aeab2e
Luca Martinelli <lucxmangajet@gmail.com>
2021-02-08 15:48:16 +0100

    added setup for WPF practice

--- PracticalLessons0912/GPR5100/WPFPractices/WPFPractices/WPFPractices/MainWindow.xaml.cs
+++ PracticalLessons0912/GPR5100/WPFPractices/WPFPractices/WPFPractices/MainWindow.xaml.cs
@@ -21,10 +21,22 @@ namespace WPFPractices
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+		public List<TodoItem> Todos { get; set; }
+		public MainWindow()
         {
+			Todos = new List<TodoItem>();
+			Todos.Add(new TodoItem() { Title = "Example 1", Completion = 0.1f });
+			Todos.Add(new TodoItem() { Title = "Learn C#", Completion = 0.8f });
+			Todos.Add(new TodoItem() { Title = "Learn Unity", Completion = 1 });
+
             InitializeComponent();
-        
-        }
-    }
+		}
+	}
+
+	public class TodoItem
+	{
+		public string Title { get; set; }
+		public float Completion { get; set; }
+	}
 }
+
